0964d979ee554c3d4bd48b79268db46e713d9454
Amanullah <amanullah128@gmail.com>
2024-07-02 12:30:27 +0500

    install react js and tailwind css and creat first tamplet

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-500 capitalize inline">
+        This is first application
+      </h1>
+    </div>
+  );
+}
+
+export default App;
